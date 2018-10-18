b03a554d3e5b299f7d7d8140d380cec195d8f5f0
sunlggggg <sunlggggg@gmail.com>
2018-10-18 19:02:26 +0800

    docs:lombok不支持jdk10

--- src/main/java/cn/hyperchain/application/Application.java
+++ src/main/java/cn/hyperchain/application/Application.java
@@ -1,6 +1,5 @@
 package cn.hyperchain.application;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
